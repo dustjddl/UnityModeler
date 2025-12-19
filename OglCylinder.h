8f99b56fd209a956b0e3f8948d469f92df92af99
dustjddl <dustjddl301@naver.com>
2025-12-19 20:41:29 +0900

    1
    1

---
+++ OglCylinder.h
@@ -0,0 +1,31 @@
+#pragma once
+#define DEF_CYL_SLICE	(30)
+#define DEF_CYL_STACK	(30)
+
+#include "OglTransform.h"
+
+class OglCylinder
+{
+public:
+	OglCylinder(void);
+	~OglCylinder();
+
+	void SetRgba(COLORREF col, GLfloat alpha = 1.0f)
+	{
+		m_nColor = col;
+		m_alpha = alpha;
+	}
+	void Create(void);
+	void Draw(GLdouble radius, GLdouble ht, GLint slice = DEF_CYL_SLICE, GLint stack = DEF_CYL_STACK) const;
+	void Draw(const OglTransform& ot);
+	void DrawDef(void);
+
+protected:
+	GLUquadric* m_pQobj;
+	COLORREF m_nColor;
+	GLfloat m_alpha;
+	const GLdouble m_defRad = 20.; // Áö¸§ = m_defRad*2
+};
+
+#undef DEF_CYL_SLICE
+#undef DEF_CYL_STACK
---
+++ OglSphere.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <GL/glew.h>
+
+#define DEF_SPH_SLICE	(30)
+#define DEF_SPH_STACK	(30)
+
+#include "OglTransform.h"
+
+class OglSphere
+{
+public:
+	OglSphere(void);
+	~OglSphere();
+
+	void SetRgba(COLORREF col, GLfloat alpha = 1.0f)
+	{
+		m_nColor = col;
+		m_alpha = alpha;
+	}
+	void Create(void);
+	void Draw(GLdouble radius, GLint slice = DEF_SPH_SLICE, GLint stack = DEF_SPH_STACK) const;
+	void Draw(const OglTransform& ot);
+	void DrawDef(void);
+
+protected:
+	GLUquadric* m_pQobj;
+	COLORREF m_nColor;
+	GLfloat m_alpha;
+	const GLdouble m_defRad = 20.; // Áö¸§ = m_defRad*2
+};
+
+#undef DEF_SPH_SLICE
+#undef DEF_SPH_STACK
+
